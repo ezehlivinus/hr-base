c0caa88bca8d72b63cec11d04f6e590317c61490
ezehlivinus <ezehlivinusezeh@gmail.com>
2024-09-05 08:17:11 +0100

    change how the app connect to database

--- hr_base_project/settings.py
+++ hr_base_project/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from datetime import timedelta
+from email.policy import default
 from pathlib import Path
 
+import dj_database_url
 # from django.conf.global_settings import STATIC_ROOT
 from dotenv import  load_dotenv
 
@@ -85,14 +87,18 @@ WSGI_APPLICATION = 'hr_base_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
-    }
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.getenv('DB_NAME'),
+        # 'USER': os.getenv('DB_USER'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD'),
+        # 'HOST': os.getenv('DB_HOST'),
+        # 'PORT': os.getenv('DB_PORT')
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
